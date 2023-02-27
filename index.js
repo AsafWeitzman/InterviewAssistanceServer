4fae0de6ceb6d5210f9aaa8033a88b2620749997
AsafWeitzman <asafweit1@gmail.com>
2023-02-28 01:37:11 +0200

    added profile picture logic using multer

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors"); // connect the server to the client (white list)
+const path = require("path");
 
+// express
 app.use(express.json()); // can response with json
+
+// cors
 app.use(cors());
 
+// app.use(express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
 const db = require("./models");
+
+const PORT = 3001;
 
 // Routers
 const interviewRouter = require("./routes/Interviews");
@@ -16,7 +25,7 @@ const usersRouter = require("./routes/Users");
 app.use("/auth", usersRouter);
 
 db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
+  app.listen(PORT, () => {
     console.log("Server running on port 3001...");
   });
 });
--- models/Users.js
+++ models/Users.js
@@ -12,6 +12,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    profilePicture: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
   });
 
   Users.associate = (models) => {
